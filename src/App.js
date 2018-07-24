fdb7fe9e37d1c42e4d2feb3d1bf0297b09930121
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-24 15:37:55 +0300

    Add UserOutput component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <UserOutput> </UserOutput>
+        <UserOutput> </UserOutput>
       </div>
     );
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div className="userOutput">
+            <p> Line 1 </p>
+            <p> Line 2 </p>
+        </div>
+    );
+}
+
+export default userOutput;
