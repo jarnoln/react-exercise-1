cc727b42ad6d24114892a5cb927a299240794f34
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-24 15:55:31 +0300

    Save usernames in state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import UserOutput from './UserOutput/UserOutput.js';
 import UserInput from './UserInput/UserInput.js';
 
 class App extends Component {
+  state = {
+    users: [
+      { username: 'Bruce' },
+      { username: 'Clark' }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <UserOutput username='bruce'> </UserOutput>
-        <UserOutput username='clark'> </UserOutput>
+        <UserOutput username={this.state.users[0].username}> </UserOutput>
+        <UserOutput username={this.state.users[1].username}> </UserOutput>
         <UserInput> </UserInput>
       </div>
     );
